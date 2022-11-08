d0bcc284043e337a6ada1c367aa105d90bea5121
Patrick Juliano <pjuliano15@gmail.com>
2022-11-08 15:27:33 -0500

    Add various attributes to hourly weather data

--- client/src/components/CurrentWeather.js
+++ client/src/components/CurrentWeather.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import '../App.css';
 import axios from 'axios';
-import { Autocomplete, Divider, List, ListItem, ListItemText, ListItemIcon, ListSubheader, TextField, Icon } from '@mui/material';
-import { WiDaySunny, WiDaySunnyOvercast, WiNightClear, WiNightAltPartlyCloudy, WiCloud, WiCloudy, WiShowers, WiRain, WiThunderstorm, WiSnowflakeCold, WiDust } from "react-icons/wi";
+import { Autocomplete, Collapse, Divider, List, ListItem, ListItemButton, ListItemText, ListItemIcon, ListSubheader, TextField, Icon, Tooltip } from '@mui/material';
+import { WiDaySunny, WiDaySunnyOvercast, WiNightClear, WiNightAltPartlyCloudy, WiCloud, WiCloudy, WiShowers, WiRain, WiThunderstorm, WiSnowflakeCold, WiDust, WiBarometer, WiThermometer, WiHumidity, WiStrongWind, WiCloudyGusts, WiDirectionRight, WiRaindrop, WiWindy, WiWindDeg, WiSprinkle, WiSnow, WiStormWarning } from 'react-icons/wi';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { IconContext } from 'react-icons';
 import { checkNumber } from '../validation';
 
@@ -35,8 +36,17 @@ const CurrentWeather = (props) => {
 	const [ location, setLocation ] = useState(null);
 	const [ currentWeatherData, setCurrentWeatherData ] = useState(null);
 	const [ hourlyWeatherData, setHourlyWeatherData ] = useState(null);
+	const [ dailyWeatherData, setDailyWeatherData ] = useState(null);
+	const [ open, setOpen ] = useState(Array(25).fill(false));
 	const [ loading, setLoading ] = useState(true);
 	const [ error, setError ] = useState(false);
+
+	const handleClick = (index) => {
+		const newValue = !open[index];
+		const newOpen = Array(25).fill(false);
+		newOpen[index] = newValue;
+		setOpen(newOpen);
+	};
 
 	useEffect(() => {
 		async function fetchData() {
@@ -50,10 +60,10 @@ const CurrentWeather = (props) => {
 					location.coordinates.lat = checkNumber(location.coordinates.lat);
 					location.coordinates.lon = checkNumber(location.coordinates.lon);
 
-					const { data: currentWeather } = await axios.get(`http://localhost:3000/weather/current?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
+					const { data: currentWeather } = await axios.get(`http://localhost:4000/weather/current?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
 					setCurrentWeatherData(currentWeather);
 
-					const { data: hourlyWeather } = await axios.get(`http://localhost:3000/weather/hourly?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
+					const { data: hourlyWeather } = await axios.get(`http://localhost:4000/weather/hourly?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
 					setHourlyWeatherData(hourlyWeather);
 				}
 				setLoading(false);
@@ -65,7 +75,8 @@ const CurrentWeather = (props) => {
 		fetchData();
 	}, [ location ]);
 
-	function onInputChange(event, value) {
+	// Set location to value selected from Autocomplete component
+	function onInputChange(_, value) {
 		setLocation(value);
 	}
 
@@ -74,37 +85,113 @@ const CurrentWeather = (props) => {
 		{label: 'Worcester, MA', coordinates: {lat: 42.2625621, lon: -71.8018877}}
 	]
 
-	const currentHour = hourlyWeatherData !== null ? hourlyWeatherData[0].dt : null;
-
 	if (error) {
-
+		var content = <p>Error!</p>
+	} else if (loading) {
+		var content = <p>Loading...</p>
 	} else {
-		return (
+		const currentHour = hourlyWeatherData ? hourlyWeatherData[0].dt : null;
+		var content =
 			<div>
-				<h2>Current Weather</h2>
-				<div>
-					<p className='selectLabel'>Select a location:</p>
-					<Autocomplete className='selectLocation' defaultValue={location} onChange={onInputChange} options={savedLocations} renderInput={(params) => <TextField {...params} label='Location' />} sx={{width: 300}} filterOptions={(x) => x} />
-				</div>
-				<p>{loading ? 'Loading...' : currentWeatherData && currentWeatherData.temp ? `Current temperature: ${currentWeatherData.temp} °F`: 'No location selected!'}</p>
-				
-				{loading ? <p>Loading...</p> : hourlyWeatherData ? 
-					<IconContext.Provider value={{size: 50}}>
-						<List subheader={<ListSubheader>Hourly Forecast</ListSubheader>} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-							{hourlyWeatherData.slice(0, 25).map(data => (
-								<ListItem divider>
-									<ListItemIcon>
-										{getWeatherIcon(data.weather[0].icon)}
-									</ListItemIcon>
-									<ListItemText primary={Math.round(data.temp) + ' ºF'} secondary={data.dt === currentHour ? 'Now' : (new Date(data.dt * 1000)).toLocaleString('en-US', { hour: 'numeric', hour12: true })} />
-								</ListItem>
-							))}
-						</List>
-					</IconContext.Provider> :
-				<p>No location selected!</p>}
+				{
+					hourlyWeatherData &&
+						<IconContext.Provider value={{size: 50}}>
+							<List subheader={<ListSubheader>Hourly Forecast</ListSubheader>} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+								{hourlyWeatherData.slice(0, 25).map((data, index) => (
+									<div>
+										<ListItemButton divider onClick={() => handleClick(index)}>
+											<ListItemIcon>
+												{getWeatherIcon(data.weather[0].icon)}
+											</ListItemIcon>
+											<ListItemText primary={Math.round(data.temp) + ' ºF'} secondary={data.dt === currentHour ? 'Now' : (new Date(data.dt * 1000)).toLocaleString('en-US', { hour: 'numeric', hour12: true })} />
+											{open[index] ? <ExpandLess /> : <ExpandMore />}
+										</ListItemButton>
+										<Collapse in={open[index]} timeout='auto' unmountOnExit>
+											<IconContext.Provider value={{className: 'nestedListIcon', color: '#757575', size: 30}}>
+												<List component='div'>
+													{'feels_like' in data &&
+														<ListItem sx={{ pl: 5 }}>
+															<WiThermometer />
+															<ListItemText primary={`Feels Like: ${data.feels_like} ºF`} />
+														</ListItem>
+													}
+													{'pressure' in data && <ListItem sx={{ pl: 5 }}>
+														<WiBarometer />
+														<ListItemText primary={`Pressure: ${data.pressure} hPa`} />
+													</ListItem>}
+													{'humidity' in data && <ListItem sx={{ pl: 5 }}>
+														<WiHumidity />
+														<ListItemText primary={`Humidity: ${data.humidity}%`} />
+													</ListItem>}
+													{'dew_point' in data && <ListItem sx={{ pl: 5 }}>
+														<WiRaindrop />
+														<ListItemText primary={`Dew Point: ${data.dew_point} ºF`} />
+													</ListItem>}
+													{'uvi' in data && <ListItem sx={{ pl: 5 }}>
+														<WiDaySunny />
+														<ListItemText primary={`UV Index: ${data.uvi}`} />
+													</ListItem>}
+													{'clouds' in data && <ListItem sx={{ pl: 5 }}>
+														<WiCloud />
+														<ListItemText primary={`Cloudiness: ${data.clouds}%`} />
+													</ListItem>}
+													{'visibility' in data && <ListItem sx={{ pl: 5 }}>
+														<WiDust />
+														<ListItemText primary={`Visibility: ${data.visibility} m`} />
+													</ListItem>}
+													{'wind_speed' in data && <ListItem sx={{ pl: 5 }}>
+														<WiWindy />
+														<ListItemText primary={`Wind Speed: ${data.wind_speed} mph`} />
+													</ListItem>}
+													{'wind_gust' in data && <ListItem sx={{ pl: 5 }}>
+														<WiCloudyGusts />
+														<ListItemText primary={`Wind Gust: ${data.wind_gust} mph`} />
+													</ListItem>}
+													{'wind_deg' in data && <ListItem sx={{ pl: 5 }}>
+														<WiWindDeg />
+														<ListItemText primary={`Wind Direction: ${data.wind_deg}º`} />
+													</ListItem>}
+													{'pop' in data && <ListItem sx={{ pl: 5 }}>
+														<WiSprinkle />
+														<ListItemText primary={`Precipiation: ${data.pop * 10}%`} />
+													</ListItem>}
+													{'rain' in data && '1h' in data.rain && <ListItem sx={{ pl: 5 }}>
+														<WiRain />
+														<ListItemText primary={`Rain: ${data.rain['1h']} mm`} />
+													</ListItem>}
+													{'snow' in data && '1h' in data.snow && <ListItem sx={{ pl: 5 }}>
+														<WiSnow />
+														<ListItemText primary={`Snow: ${data.snow['1h']} mm`} />
+													</ListItem>}
+													{'weather' in data && 'main' in data.weather && <ListItem sx={{ pl: 5 }}>
+														<WiStormWarning />
+														<ListItemText primary={`Weather: ${data.weather.main}`} />
+													</ListItem>}
+												</List>
+											</IconContext.Provider>
+										</Collapse>
+									</div>
+								))}
+							</List>
+						</IconContext.Provider>
+				}
+				{
+					dailyWeatherData &&
+						<p>TODO: DAILY WEATHER</p>
+				}
 			</div>
-		);
 	}
+
+	return (
+		<div>
+			<h2>Current Weather</h2>
+			<div>
+				<p className='selectLabel'>Select a location:</p>
+				<Autocomplete className='selectLocation' defaultValue={location} onChange={onInputChange} options={savedLocations} renderInput={(params) => <TextField {...params} label='Location' />} sx={{width: 300}} filterOptions={(x) => x} />
+			</div>
+			{content}
+		</div>
+	);
 };
 
 export default CurrentWeather;
