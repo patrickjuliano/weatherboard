1ead40d9f934680fa8f7d0575f08f943dbe0cc01
Patrick Juliano <pjuliano15@gmail.com>
2022-11-09 11:37:05 -0500

    Add hourly forecast data and refactor attribute generation

--- client/src/components/CurrentWeather.js
+++ client/src/components/CurrentWeather.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Autocomplete, Collapse, Divider, List, ListItem, ListItemButton, ListItemText, ListItemIcon, ListSubheader, TextField, Icon, Tooltip, Tabs, Tab } from '@mui/material';
-import { WiDaySunny, WiDaySunnyOvercast, WiNightClear, WiNightAltPartlyCloudy, WiCloud, WiCloudy, WiShowers, WiRain, WiThunderstorm, WiSnowflakeCold, WiDust, WiBarometer, WiThermometer, WiHumidity, WiStrongWind, WiCloudyGusts, WiDirectionRight, WiRaindrop, WiWindy, WiWindDeg, WiSprinkle, WiSnow, WiStormWarning } from 'react-icons/wi';
+import { WiDaySunny, WiDaySunnyOvercast, WiNightClear, WiNightAltPartlyCloudy, WiCloud, WiCloudy, WiShowers, WiRain, WiThunderstorm, WiSnowflakeCold, WiDust, WiBarometer, WiThermometer, WiHumidity, WiStrongWind, WiCloudyGusts, WiDirectionRight, WiRaindrop, WiWindy, WiWindDeg, WiSprinkle, WiSnow, WiStormWarning, WiSunrise, WiSunset, WiMoonrise, WiMoonset, WiMoonFull, WiMoonNew, WiMoonWaningCrescent1, WiMoonFirstQuarter, WiMoonWaxingGibbous1, WiMoonWaningGibbous1, WiMoonThirdQuarter, WiMoonWaxingCrescent1 } from 'react-icons/wi';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { IconContext } from 'react-icons';
 import { checkNumber } from '../validation';
@@ -38,6 +38,7 @@ const CurrentWeather = (props) => {
 	const [ currentWeatherData, setCurrentWeatherData ] = useState(null);
 	const [ hourlyWeatherData, setHourlyWeatherData ] = useState(null);
 	const [ dailyWeatherData, setDailyWeatherData ] = useState(null);
+	const [ historicalWeatherData, setHistoricalWeatherData ] = useState(null);
 	const [ tabIndex, setTabIndex ] = useState(null);
 	const [ open, setOpen ] = useState(Array(25).fill(false));
 	const [ loading, setLoading ] = useState(false);
@@ -73,6 +74,9 @@ const CurrentWeather = (props) => {
 
 					const { data: hourlyWeather } = await axios.get(`http://localhost:4000/weather/hourly?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
 					setHourlyWeatherData(hourlyWeather);
+
+					const { data: dailyWeather } = await axios.get(`http://localhost:4000/weather/daily?lat=${location.coordinates.lat}&lon=${location.coordinates.lon}`);
+					setDailyWeatherData(dailyWeather);
 				} catch (e) {
 					alert(e);
 				}
@@ -93,6 +97,256 @@ const CurrentWeather = (props) => {
 		setLocation(value);
 	}
 
+	// Get moon phase based on number between 0 and 1
+	function getMoonPhase(value) {
+		if (value === 0 || value === 1) {
+			return { name: 'New Moon', icon: <WiMoonNew /> };
+		} else if (value < .25) {
+			return { name: 'Waxing Crescent', icon: <WiMoonWaxingCrescent1 /> };
+		} else if (value === .25) {
+			return { name: 'First Quarter Moon', icon: <WiMoonFirstQuarter /> };
+		} else if (value < .5) {
+			return { name: 'Waxing Gibbous', icon: <WiMoonWaxingGibbous1 /> };
+		} else if (value === .5) {
+			return { name: 'Full Moon', icon: <WiMoonFull /> };
+		} else if (value < .75) {
+			return { name: 'Waning Gibbous', icon: <WiMoonWaningGibbous1 /> };
+		} else if (value === .75) {
+			return { name: 'Third Quarter Moon', icon: <WiMoonThirdQuarter /> };
+		} else {
+			return { name: 'Waning Crescent', icon: <WiMoonWaningCrescent1 /> };
+		}
+	}
+
+	// Create attribute with specified properties
+	function createAttribute(tooltip, icon, label, value, units = '', addSpace = false) {
+		let text = `${label}: ${value}${addSpace ? ' ' : ''}${units}`;
+		return (
+			<Tooltip title={tooltip} placement='right' arrow >
+				<ListItemButton sx={{ pl: 5 }}>
+					{icon}
+					<ListItemText primary={text} />
+				</ListItemButton>
+			</Tooltip>
+		);
+	}
+
+	// Acquire attributes for a given weather data object
+	function createAttributeList(data) {
+		switch (tabIndex) {
+			case (0):
+				return (
+					<List component='div'>
+						{'feels_like' in data &&
+							createAttribute(
+								'Temperature accounting for the human perception of weather',
+								<WiThermometer />,
+								'Feels Like',
+								data.feels_like,
+								'ºF',
+								true
+							)
+						}
+						{'pressure' in data &&
+							createAttribute(
+								'Atmospheric pressure on the sea level',
+								<WiBarometer />,
+								'Pressure',
+								data.pressure,
+								'hPa',
+								true
+							)
+						}
+						{'humidity' in data &&
+							createAttribute(
+								'Amount of water vapor in the air',
+								<WiHumidity />,
+								'Humidity',
+								data.humidity,
+								'%'
+							)
+						}
+						{'dew_point' in data &&
+							createAttribute(
+								'Atmospheric temperature (varying according to pressure and humidity) below which water droplets begin to condense and dew can form',
+								<WiRaindrop />,
+								'Dew Point',
+								data.dew_point,
+								'ºF',
+								true
+							)
+						}
+						{'uvi' in data &&
+							createAttribute(
+								'Strength of ultraviolet radiation',
+								<WiDaySunny />,
+								'UV Index',
+								data.uvi
+							)
+						}
+						{'clouds' in data &&
+							createAttribute(
+								'Measure of cloudiness',
+								<WiCloud />,
+								'Clouds',
+								data.clouds,
+								'%'
+							)
+						}
+						{'visibility' in data &&
+							createAttribute(
+								'Average visibility (capped at 10 km)',
+								<WiDust />,
+								'Visibility',
+								data.visibility,
+								'm',
+								true
+							)
+						}
+						{'wind_speed' in data &&
+							createAttribute(
+								'Speed at which wind travels',
+								<WiWindy />,
+								'Wind Speed',
+								data.wind_speed,
+								'mph',
+								true
+							)
+						}
+						{'wind_gust' in data &&
+							createAttribute(
+								'Peak wind speed',
+								<WiCloudyGusts />,
+								'Wind Gust',
+								data.wind_gust,
+								'mph',
+								true
+							)
+						}
+						{'wind_deg' in data &&
+							createAttribute(
+								'Direction from which wind travels',
+								<WiWindDeg />,
+								'Wind Direction',
+								data.wind_deg,
+								'º'
+							)
+						}
+						{'pop' in data &&
+							createAttribute(
+								'Probability of precipitation',
+								<WiSprinkle />,
+								'Precipiation',
+								data.pop * 10,
+								'%'
+							)
+						}
+						{'rain' in data && '1h' in data.rain &&
+							createAttribute(
+								'Rain volume',
+								<WiRain />,
+								'Rain',
+								data.rain['1h'],
+								'mm',
+								true
+							)
+						}
+						{'snow' in data && '1h' in data.snow &&
+							createAttribute(
+								'Snow volume',
+								<WiSnow />,
+								'Snow',
+								data.snow['1h'],
+								'mm',
+								true
+							)
+						}
+						{'weather' in data && 'main' in data.weather &&
+							createAttribute(
+								'Weather condition',
+								<WiStormWarning />,
+								'Weather',
+								data.weather.main
+							)
+						}
+					</List>
+				);
+			case (1):
+				return (
+					<List component='div'>
+						{'sunrise' in data &&
+							createAttribute(
+								'Time of sunrise',
+								<WiSunrise />,
+								'Sunrise',
+								data.sunrise,
+								'UNITS',
+								true
+							)
+						}
+						{'sunset' in data &&
+							createAttribute(
+								'Time of sunset',
+								<WiSunset />,
+								'Sunset',
+								data.sunset,
+								'UNITS',
+								true
+							)
+						}
+						{'moonrise' in data &&
+							createAttribute(
+								'Time of moonrise',
+								<WiMoonrise />,
+								'Moonrise',
+								data.moonrise,
+								'UNITS',
+								true
+							)
+						}
+						{'moonset' in data &&
+							createAttribute(
+								'Time of moonset',
+								<WiMoonset />,
+								'Moonset',
+								data.moonset,
+								'UNITS',
+								true
+							)
+						}
+						{'moon_phase' in data &&
+							createAttribute(
+								'Phase of the moon',
+								getMoonPhase(data.moon_phase).icon,
+								'Moon Phase',
+								getMoonPhase(data.moon_phase).name
+							)
+						}
+						{'temp' in data && 'min' in data.temp &&
+							createAttribute(
+								'Minimum daily temperature',
+								<WiThermometer />,
+								'Low Temperature',
+								data.temp.min,
+								'ºF',
+								true
+							)
+						}
+						{'temp' in data && 'max' in data.temp &&
+							createAttribute(
+								'Maximum daily temperature',
+								<WiThermometer />,
+								'High Temperature',
+								data.temp.max,
+								'ºF',
+								true
+							)
+						}
+					</List>
+				);
+		}
+	}
+
 	const savedLocations = [
 		{label: 'Hoboken, NJ', coordinates: {lat: 40.744052, lon: -74.0270745}}, 
 		{label: 'Worcester, MA', coordinates: {lat: 42.2625621, lon: -71.8018877}}
@@ -103,148 +357,41 @@ const CurrentWeather = (props) => {
 	} else if (loading) {
 		var content = <p>Loading...</p>
 	} else {
-		const currentHour = hourlyWeatherData ? hourlyWeatherData[0].dt : null;
+		if (tabIndex === 0 && hourlyWeatherData) {
+			var weatherData = hourlyWeatherData;
+			var header = 'Hourly Forecast';
+			var end = 25;
+			var current = hourlyWeatherData[0].dt;
+		} else if (tabIndex === 1 && dailyWeatherData) {
+			var weatherData = dailyWeatherData;
+			var header = 'Daily Forecast';
+			var end = 8;
+			var current = null; // TODO
+		}
 		var content =
 			<div>
 				{
-					tabIndex === 0 && hourlyWeatherData &&
+					weatherData &&
 						<IconContext.Provider value={{size: 50}}>
-							<List subheader={<ListSubheader>Hourly Forecast</ListSubheader>} sx={{ width: '100%', maxWidth: 300, bgcolor: 'background.paper' }}>
-								{hourlyWeatherData.slice(0, 25).map((data, index) => (
+							<List subheader={<ListSubheader>{header}</ListSubheader>} sx={{ width: '100%', maxWidth: 300, bgcolor: 'background.paper' }}>
+								{weatherData.slice(0, end).map((data, index) => (
 									<div>
 										<ListItemButton divider onClick={() => handleClick(index)}>
 											<ListItemIcon>
 												{getWeatherIcon(data.weather[0].icon)}
 											</ListItemIcon>
-											<ListItemText primary={Math.round(data.temp) + ' ºF'} secondary={data.dt === currentHour ? 'Now' : (new Date(data.dt * 1000)).toLocaleString('en-US', { hour: 'numeric', hour12: true })} />
+											<ListItemText primary={Math.round(data.temp) + ' ºF'} secondary={data.dt === current ? 'Now' : (new Date(data.dt * 1000)).toLocaleString('en-US', { hour: 'numeric', hour12: true })} />
 											{open[index] ? <ExpandLess /> : <ExpandMore />}
 										</ListItemButton>
 										<Collapse in={open[index]} timeout='auto' unmountOnExit>
-											<IconContext.Provider value={{className: 'nestedListIcon', size: 30 }}>
-												<List component='div'>
-													{'feels_like' in data &&
-														<Tooltip title='Temperature accounting for the human perception of weather' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiThermometer />
-																<ListItemText primary={`Feels Like: ${data.feels_like} ºF`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'pressure' in data && 
-														<Tooltip title='Atmospheric pressure on the sea level' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiBarometer />
-																<ListItemText primary={`Pressure: ${data.pressure} hPa`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'humidity' in data && 
-														<Tooltip title='Amount of water vapor in the air' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiHumidity />
-																<ListItemText primary={`Humidity: ${data.humidity}%`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'dew_point' in data && 
-														<Tooltip title='Atmospheric temperature (varying according to pressure and humidity) below which water droplets begin to condense and dew can form' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiRaindrop />
-																<ListItemText primary={`Dew Point: ${data.dew_point} ºF`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'uvi' in data && 
-														<Tooltip title='Strength of ultraviolet radiation' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiDaySunny />
-																<ListItemText primary={`UV Index: ${data.uvi}`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'clouds' in data && 
-														<Tooltip title='Measure of cloudiness' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiCloud />
-																<ListItemText primary={`Clouds: ${data.clouds}%`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'visibility' in data && 
-														<Tooltip title='Average visibility (capped at 10 km)' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiDust />
-																<ListItemText primary={`Visibility: ${data.visibility} m`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'wind_speed' in data && 
-														<Tooltip title='Speed at which wind travels' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiWindy />
-																<ListItemText primary={`Wind Speed: ${data.wind_speed} mph`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'wind_gust' in data && 
-														<Tooltip title='Peak wind speed' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiCloudyGusts />
-																<ListItemText primary={`Wind Gust: ${data.wind_gust} mph`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'wind_deg' in data && 
-														<Tooltip title='Direction from which wind travels' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiWindDeg />
-																<ListItemText primary={`Wind Direction: ${data.wind_deg}º`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'pop' in data && 
-														<Tooltip title='Probability of precipitation' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiSprinkle />
-																<ListItemText primary={`Precipiation: ${data.pop * 10}%`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'rain' in data && '1h' in data.rain && 
-														<Tooltip title='Rain volume' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiRain />
-																<ListItemText primary={`Rain: ${data.rain['1h']} mm`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'snow' in data && '1h' in data.snow && 
-														<Tooltip title='Snow volume' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiSnow />
-																<ListItemText primary={`Snow: ${data.snow['1h']} mm`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-													{'weather' in data && 'main' in data.weather && 
-														<Tooltip title='Weather condition' placement='right' arrow >
-															<ListItemButton sx={{ pl: 5 }}>
-																<WiStormWarning />
-																<ListItemText primary={`Weather: ${data.weather.main}`} />
-															</ListItemButton>
-														</Tooltip>
-													}
-												</List>
+											<IconContext.Provider value={{className: 'nestedListIcon', color: '#757575', size: 30 }}>
+												{createAttributeList(data)}
 											</IconContext.Provider>
 										</Collapse>
 									</div>
 								))}
 							</List>
 						</IconContext.Provider>
-				}
-				{
-					tabIndex === 1 && dailyWeatherData &&
-						<p>TODO: DAILY WEATHER</p>
 				}
 			</div>
 	}
@@ -259,6 +406,7 @@ const CurrentWeather = (props) => {
 					<Tabs value={tabIndex} onChange={selectTab}>
 						<Tab label="Hourly Forecast" disabled={!location} />
 						<Tab label="Daily Forecast" disabled={!location} />
+						<Tab label="Historical Data" disabled={!location} />
 					</Tabs>
 				</Box>
 			</div>
