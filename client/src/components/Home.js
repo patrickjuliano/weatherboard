3b946095f85745e764429dc297f358b254b1c804
Patrick Juliano <pjuliano15@gmail.com>
2022-12-16 15:17:47 -0500

    Add labels to links on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,12 +11,12 @@ const Home = ({ currentUserID }) => {
 			</p>
 			{currentUserID ?
 				<div>
-					<p>To add or remove saved locations, click <Link to='/locations'>here.</Link></p>
-					<p>To view real-time and forecasted weather data for your saved locations, click <Link to='/weather/current'>here.</Link></p>
-					<p>To edit your credentials, click <Link to='/credentials'>here.</Link></p>
+					<p>To add or remove saved locations, click <Link to='/locations' aria-label='Locations'>here.</Link></p>
+					<p>To view real-time and forecasted weather data for your saved locations, click <Link to='/weather/current' aria-label='Forecast'>here.</Link></p>
+					<p>To edit your credentials, click <Link to='/credentials' aria-label='Credentials'>here.</Link></p>
 				</div>
 			:
-				<p>To get started, sign up or log in <Link to='/login'>here.</Link></p>
+				<p>To get started, sign up or log in <Link to='/login' aria-label='Log In or Sign Up'>here.</Link></p>
 			}
 		</div>
 	);
