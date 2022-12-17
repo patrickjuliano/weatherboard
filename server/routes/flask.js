2336fd085a7c538bd6b2b7f7f1ee47df9f8b79b1
Patrick Cullinane <cullinap@gmail.com>
2022-12-17 17:40:34 -0500

    update flask route to accept city and days

--- server/routes/flask.js
+++ server/routes/flask.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/data', async (req, res) => {
+router.get('/data/:city/:periods', async (req, res) => {
     try {
-        let flask_data = await axios.get('http://127.0.0.1:8000/');
-        console.log(flask_data);
+        let city = req.params.city;
+        let periods = parseInt(req.params.periods);
+        let flask_data = await axios.get(`http://127.0.0.1:8000/` + city + '/' + periods);
         res.json({ home: flask_data.data });
     }catch (e) {
         res.status(404).json({ error: e });
