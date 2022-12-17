650624e4c611b95d285bcc22418f77ec0c998ffc
Patrick Cullinane <cullinap@gmail.com>
2022-12-17 13:41:22 -0500

    udpate app to include multiple cities

--- flask_server/my_app/views.py
+++ flask_server/my_app/views.py
@@ -5,19 +5,30 @@ import pandas as pd
 from datetime import date, datetime, timedelta
 from flask import Response
 
-def load_model():
-    with open('./train_model/serialized_model.json', 'r') as fin:
-        m = model_from_json(json.load(fin)) 
+city_map = {
+    'Austin': 'austin',
+    'miami': 'miami',
+    'New York': 'newyork',
+    'San Francisco': 'sanfran',
+    'Washington': 'washington'
+}
 
+def load_model(city):
+    with open(f'./models/{city}.json', 'r') as fin:
+        m = model_from_json(json.load(fin)) 
     return m
+
+def map_city(city):
+    return city_map[city]
 
 def set_periods(cur_date):
     last_train_date = date(2022, 12, 11)
     return cur_date - last_train_date
 
-@app.route("/<periods>", methods=["GET", "POST"])
-def hello_world(periods):
-    m = load_model()
+@app.route("/<city>/<periods>")
+def hello_world(city=None, periods=None):
+    city_mapped = map_city(city) # remap the city
+    m = load_model(city_mapped)
     day_adder = set_periods(date(2022, 12, 16)).days
     future = m.make_future_dataframe(periods=int(periods)+day_adder)
     forecast = m.predict(future)
